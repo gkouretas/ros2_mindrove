6cb450abf0f67a239fa09ee602f045ee7b82b9ed
gkouretas <thegreekgeorge07@gmail.com>
2024-12-12 03:59:28 -0800

    Adjusted scripts to initialize node locally

--- ros2_mindrove/scripts/mindrove_armband_logger_node.py
+++ ros2_mindrove/scripts/mindrove_armband_logger_node.py
@@ -7,11 +7,14 @@ import rclpy
 # Project based / ROS imports
 from ros2_mindrove.mindrove_configs import *
 from ros2_mindrove.mindrove_armband_logger import ROSMindroveCSVLogger
-from python_utils.ros2_utils.comms.node_manager import get_node
+from python_utils.ros2_utils.comms.node_manager import create_simple_node
 
 def main() -> None:
+    rclpy.init()
+    node = create_simple_node(MINDROVE_ROS_NODE)
+
     ROSMindroveCSVLogger()
-    rclpy.spin(get_node(MINDROVE_ROS_NODE))
+    rclpy.spin(node)
 
 if __name__ == "__main__":
     main()
--- ros2_mindrove/scripts/mindrove_armband_publisher_node.py
+++ ros2_mindrove/scripts/mindrove_armband_publisher_node.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+import rclpy
+
 from mindrove_typedefs import *
 from mindrove_armband_publisher import MindroveArmbandRosPublisher
 from mindrove.utils import LogLevels
 
+from python_utils.ros2_utils.comms.node_manager import create_simple_node
+from mindrove_configs import *
+
 def main() -> None:
+   rclpy.init()
+   node = create_simple_node(MINDROVE_ROS_NODE)
+
    interface = MindroveArmbandRosPublisher(
       simulated = False,
       additional_channels = ("timestamp", "battery"),
@@ -12,9 +20,11 @@ def main() -> None:
 
    interface.run(
       num_samples = 200, 
-      nonblocking = False,
+      nonblocking = True,
       lag_compensation = True
    )
+
+   rclpy.spin(node)
 
 if __name__ == "__main__":
    main()
